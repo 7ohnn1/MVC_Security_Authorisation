558dc8535a9af8d07b645929460e59e6e17718a3
7ohnn1 <7ohnn1@gmail.com>
2016-10-20 16:14:29 +0800

    Basic Setup Completed ; ready to move to ASP.Identity

---
+++ Libraries/Claims.Core/Infrastructure/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Claims.Core.Infrastructure
+{
+    public class AppUser : IdentityUser
+    {
+        // additional properties will go here
+    }
+}
---
+++ Libraries/Claims.Data/AppIdentityDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Claims.Core.Infrastructure;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Claims.Data
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDb")
+        {
+        }
+
+/*
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+*/
+
+        //instance of the class are created via here when needed by the OWIN
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+
+    }
+
+}
